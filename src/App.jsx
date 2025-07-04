15a764d8db505449e2293547e33f9f36f0277734
RiteshDev99 <riteshchaudhary5191@gmail.com>
2025-07-04 16:17:18 +0530

    implement redux and header footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import './App.css'
+import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import authServices from "./appwrite/authServices.js";
+import {login, logout} from './store/feature/auth/authSlice.js'
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
 
 function App() {
-  return (
-    <h2>Welcome To MegaProject....</h2>
-  )
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServices.getCurrentUser()
+        .then((userData)=>{
+          if (userData){
+            dispatch(login({userData}))
+          }else{
+            dispatch(logout())
+          }
+        })
+        .catch((error)=>{
+            console.log("user not found", error)
+            setLoading(false)
+        })
+        .finally(()=>setLoading(false))
+  },[])
+
+
+  return !loading ?(
+              <div className='min-h-screen bg-gray-700 w-[100%] flex flex-wrap content-between justify-center'>
+                  <Header/>
+                  <min className="mincontainer bg-green-700 w-[100%]">
+                  </min>
+                  <Footer/>
+              </div>
+          ):null
+
 }
 
 export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <h2 className='text-white text-3xl'>Header</h2>
+    )
+}
+
+export default Header
+
